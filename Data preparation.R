42263e9ba3326a03ea3d41b277ed59027847ddf9
Shriprabha Upadhyaya <100106974+shriprabhau@users.noreply.github.com>
2024-02-20 13:14:35 +0800

    Update Data preparation.R

--- Data preparation.R
+++ Data preparation.R
@@ -1,5 +1,19 @@
 #R script
 conda activate r-env
 Library(rMVP)
-geno <- ('Genotype_file')
+library(rMVP)
+MVP.Data(fileNum="numeric.txt",
+                  filePhe="trait.txt",
+                          fileMap="map.txt",
+                           #sep.num="\t",
+                                    #sep.map="\t",
+                                    #sep.phe="\t",
+                                             fileKin=FALSE,
+                                             filePC=FALSE,
+                                                      #priority="memory",
+                                                      #maxLine=10000,
+                                                      out="mvp"
+                                                      )
 
+#check your geno.map file, to have the header in caps - SNP CHROM and POS 
+#else it reads it as another marker and throws the error markers in genotype and map don't match, which is extremely weird if you ask me 
